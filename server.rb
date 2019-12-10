a17c3b1e360b6e2176954a57c8fa33f9cdefa43a
Jomir Uddin <juddin@nyc.yearup.org>
2019-12-10 16:04:35 -0500

    first commit

---
+++ server.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'googlebooks'
+
+get '/' do
+    erb :index
+end
+
+post '/' do
+    @input = params['query']
+    @results = GoogleBooks.search(@input)
+    erb :results
+end
